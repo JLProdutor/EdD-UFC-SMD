b54058ab4ab1f0bd29a945b343bbcccafc6e6b44
JLProdutor <joaolucaslima0207@gmail.com>
2026-09-02 15:42:00 -0300

    Conteúdo da aula de hoje - 02/09/2026

---
+++ estruturas_lineares/lineares.py
@@ -0,0 +1,21 @@
+''' --- Estruturas Lineares ---
+Funcionam de maneira linear (ordem), com posições (index)
+
+Tipos: Lista, Fila, Pilha, Deque
+
+Como são inseridos e removidos?
+Fim/Base -----------> Início/Topo
+
+Lista: Qualquer | Qualquer   
+    Inserção Não Destrutiva - não apaga, arranja espaço
+    Remoção Sem Buracos - valores assumem os espaços vazios
+    Variação: Lista Circular
+
+Fila: Fim | Início           
+    Variação: Fila com Prioridade - Furo | Início
+
+Pilha: Topo | Topo
+
+DEQUE (Double Ended Que): Fim/Início | Fim/Início
+    Junção de Pilha e Fila
+'''
