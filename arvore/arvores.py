b047df0d0dbb2e1e69ff339424a8d51a0105fc6e
JLProdutor <joaolucaslima0207@gmail.com>
2026-09-23 15:49:30 -0300

    Conteúdo da aula de hoje: 23/09/2026

---
+++ arvore/arvores.py
@@ -0,0 +1,27 @@
+'''
+----- Árvores -----
+Consegue representar hierarquias (algo influencia algo)
+Pode possuir múltiplas ("n") ramificações ("filhos") vindas de um mesmo ponto ("nó/pai")
+Tudo na árvore provém da raiz ("primeiro pai")
+Cada nó só pode possuir 1 "pai" | Cada "pai" pode possuir múltiplos "filhos"
+Nó interno: Possui "filho" | Nó externo: Não possui "filho"
+Nós com o mesmo "pai" são nós "irmãos" | Se estão no mesmo nível com "pais" diferentes, não são
+Arestas: Dois pontos na árvore 
+Caminho: Ligação entre as arestas (determinar uma ação, ou parte dela)
+Sub-árvore: Uma estrutura árvore dentro de outra
+Ordem do caminho: Depende do caso em que a estrutura for utilizada
+Ordem "mais utilizada": Profundidade Maior -> Menor & Esquerda -> Direita
+
+----- Árvore Binária -----
+Cada nó só possui 2 "filhos"
+Árvore de Busca Binária: O filho da esquerda precede o da direita 
+A ordem importa (relação menor ou maior que o "pai")
+
+----- Grafos -----
+Consegue representar rotas
+Existe caso possua mais de um "pai"
+Pode possuir múltiplos "pais" e "filhos"
+CUIDADO!!! ISSO NÃO É UMA ÁRVORE!!!
+
+Exemplos:
+'''
